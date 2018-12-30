2fabfadda0a104438347504032d94ad2b90b14fb
Sara Varisara <varisara.n@gmail.com>
2018-12-30 04:35:13 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,11 @@
-def turn_count
+def turn_count(board)
   counter = 0
   if my_condition_is_met
     counter += 1
   end
 
 def current_player
-  if turn_count % 2 == 0
+  if turn_count(board) % 2 == 0
     puts "X"
   else
     puts "O"
